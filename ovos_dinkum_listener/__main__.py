221c4af737335ca707924ca1d971ac4e5f1079f9
Daniel McKnight <daniel@neongecko.com>
2023-05-31 13:35:11 -0700

    Update `main` method to match ovos-listener for compatibility Closes #42

--- ovos_dinkum_listener/__main__.py
+++ ovos_dinkum_listener/__main__.py
@@ -9,16 +9,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-from ovos_config import Configuration
+
 from ovos_utils import wait_for_exit_signal
 
-from ovos_dinkum_listener.service import OVOSDinkumVoiceService
+from ovos_dinkum_listener.service import OVOSDinkumVoiceService, on_ready, \
+    on_error, on_stopping
 
 
-def main():
+def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping,
+         watchdog=lambda: None):
     """Service entry point"""
-    service = OVOSDinkumVoiceService()
+    service = OVOSDinkumVoiceService(on_ready=ready_hook, on_error=error_hook,
+                                     on_stopping=stopping_hook,
+                                     watchdog=watchdog)
     service.start()
     wait_for_exit_signal()
     service.stop()
