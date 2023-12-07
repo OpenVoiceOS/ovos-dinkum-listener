503d9c8b7f84aa8e21b07f6548a66c42f2066918
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-12-07 16:37:58 +0000

    Update voice_loop.py

--- ovos_dinkum_listener/voice_loop/voice_loop.py
+++ ovos_dinkum_listener/voice_loop/voice_loop.py
@@ -604,7 +604,7 @@ class DinkumVoiceLoop(VoiceLoop):
         @return: validated language (or default)
         """
         default_lang = Configuration().get("lang", "en-us")
-        valid_langs = [l.lower().split("-")[0] for l in [ [default_lang] + Configuration().get("secondary_langs", [])]
+        valid_langs = [l.lower().split("-")[0] for l in [default_lang] + Configuration().get("secondary_langs", [])]
         l2 = lang.lower().split("-")[0]
         if l2 in valid_langs:
             if l2 != default_lang.lower().split("-")[0]:
