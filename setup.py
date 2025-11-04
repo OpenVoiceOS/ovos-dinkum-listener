c27068eade29a27062ac4ab35f153455b6573429
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-11-04 14:53:22 +0000

    Default to onnx (#193)
    * feat: default to onnx plugins
    
    * feat: default to onnx plugins
    
    * feat: default to onnx plugins

--- setup.py
+++ setup.py
@@ -84,7 +84,6 @@ setup(
     extras_require={
         "extras": required("requirements/extras.txt"),
         "linux": required("requirements/linux.txt"),
-        "onnx": required("requirements/onnx.txt"),
         "tests": required("requirements/tests.txt"),
     },
     classifiers=[
